a86dd43de2abfe643cdd25d04d995864391a8a78
Riley Paul <rileypaul96@gmail.com>
2023-02-09 06:01:53 -0800

    style login and logout components

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,14 +1,29 @@
 <script lang="ts">
   import "./app.css";
+
+  import { currentUser, pb } from "./lib/pocketbase";
+
+  import Login from "./components/Login.svelte";
   import Lists from "./components/Lists.svelte";
   import Items from "./components/Items.svelte";
   import PackingList from "./components/PackingList.svelte";
 </script>
 
 <main>
-  <Lists />
-  <Items />
-  <PackingList />
+  {#if $currentUser}
+    <div class="flex justify-between items-center p-2">
+      <p>Signed in as {$currentUser.username}</p>
+      <button
+        on:click={() => pb.authStore.clear()}
+        class="px-2 py-1 rounded bg-slate-200">Sign Out</button
+      >
+    </div>
+    <Lists />
+    <Items />
+    <PackingList />
+  {:else}
+    <Login />
+  {/if}
 </main>
 
 <!-- <main class="p-2">
--- client/src/components/Login.svelte
+++ client/src/components/Login.svelte
@@ -26,11 +26,36 @@
   }
 </script>
 
-<form on:submit|preventDefault>
-  <input placeholder="Username" type="text" bind:value={username} />
-  <input placeholder="Password" type="password" bind:value={password} />
-  <button class="px-2 py-1 rounded bg-slate-200" on:click={signUp}
-    >Sign Up</button
+<div class="flex justify-center items-center h-screen bg-slate-900">
+  <form
+    on:submit|preventDefault
+    class="flex flex-col bg-slate-800 p-8 gap-4 w-3/4"
   >
-  <button class="px-2 py-1 rounded bg-slate-200" on:click={login}>Login</button>
-</form>
+    <div class="text-white">
+      <h1 class="font-bold text-2xl">Welcome to PackLighter</h1>
+      <p class="text-gray-300">Please login to start optimizing your packing lists!</p>
+    </div>
+    <input
+      placeholder="Username"
+      type="text"
+      bind:value={username}
+      class="w-full p-2 rounded"
+    />
+    <input
+      placeholder="Password"
+      type="password"
+      bind:value={password}
+      class="w-full p-2 rounded"
+    />
+    <div class="w-full flex gap-4">
+      <button
+        class="px-2 py-1 rounded bg-blue-500 text-white w-full"
+        on:click={login}>Login</button
+      >
+      <button
+        class="px-2 py-1 rounded bg-blue-500 text-white w-full"
+        on:click={signUp}>Sign Up</button
+      >
+    </div>
+  </form>
+</div>
