8a3568e7d44feadee6c5cd2740b82e5e99088ca0
Riley Paul <rileypaul96@gmail.com>
2023-09-25 17:50:53 -0700

    commit all

--- client-vite-react/src/router.tsx
+++ client-vite-react/src/router.tsx
@@ -3,34 +3,18 @@ import { RouteObject, createBrowserRouter } from "react-router-dom";
 import { ErrorPage } from "./components/Error.tsx";
 
 import { WithNavbar } from "./layouts/WithNavbar.tsx";
-import { WithSidebar } from "./layouts/WithSidebar.tsx";
 
-import { Welcome } from "./pages/Welcome.tsx";
 import { Auth } from "./pages/Auth.tsx";
-import { List } from "./pages/List.tsx";
 
 const routes: RouteObject[] = [
-  // {
-  //   path: "/",
-  //   ErrorBoundary: ErrorPage,
-  //   element: <Welcome />,
-  // },
   {
+    path: "/",
     element: <WithNavbar />,
     ErrorBoundary: ErrorPage,
     children: [
       {
         path: "/auth",
         element: <Auth />,
-      },
-      {
-        element: <WithSidebar />,
-        children: [
-          {
-            path: "/",
-            element: <List />,
-          },
-        ],
       },
     ],
   },
