a3023bec9a091dfc6274065b52f3d76ee66c225d
Riley Paul <rileypaul96@gmail.com>
2023-11-23 15:06:35 -0800

    add shadcn to sveltekit project

--- sveltekit-pb/src/routes/+page.svelte
+++ sveltekit-pb/src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script>
+	import Button from '$lib/components/ui/button/button.svelte';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Button>Hello</Button>
