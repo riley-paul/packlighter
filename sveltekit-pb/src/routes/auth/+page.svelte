b3d650b091394bb2d681d09cf57e68ae35d29c36
Riley Paul <rileypaul96@gmail.com>
2023-11-23 15:50:50 -0800

    fix up styling of login page

--- sveltekit-pb/src/routes/auth/+page.svelte
+++ sveltekit-pb/src/routes/auth/+page.svelte
@@ -5,6 +5,7 @@
     TabsList,
     TabsTrigger,
   } from "$components/ui/tabs";
+  import { Feather } from "lucide-svelte";
   import type { PageServerData } from "./$types";
 
   export let data: PageServerData;
@@ -13,15 +14,23 @@
   import SignUpForm from "./SignUpForm.svelte";
 </script>
 
-<Tabs value="login" class="max-w-md mx-auto py-8">
-  <TabsList class="grid w-full grid-cols-2">
-    <TabsTrigger value="login">Login</TabsTrigger>
-    <TabsTrigger value="signup">Sign Up</TabsTrigger>
-  </TabsList>
-  <TabsContent value="login">
-    <LoginForm {data} />
-  </TabsContent>
-  <TabsContent value="signup">
-    <SignUpForm {data} />
-  </TabsContent>
-</Tabs>
+<div class="container max-w-sm">
+  <div class="flex flex-col py-8 items-center">
+    <Feather class="h-16 w-16 text-teal-500 mb-4" />
+    <h1 class="text-3xl font-bold mb-1">PackLighter</h1>
+    <p class="text-sm text-muted-foreground">The packing tool of champions</p>
+  </div>
+
+  <Tabs value="login">
+    <TabsList class="grid w-full grid-cols-2">
+      <TabsTrigger value="login">Login</TabsTrigger>
+      <TabsTrigger value="signup">Sign Up</TabsTrigger>
+    </TabsList>
+    <TabsContent value="login">
+      <LoginForm {data} />
+    </TabsContent>
+    <TabsContent value="signup">
+      <SignUpForm {data} />
+    </TabsContent>
+  </Tabs>
+</div>
