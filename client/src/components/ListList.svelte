38efe8d5d6abced06f1e1e4d958f5080edb7d5b2
Riley Paul <rileypaul96@gmail.com>
2023-12-14 08:17:07 -0800

    add ghost when dragging to list of lists

--- client/src/components/ListList.svelte
+++ client/src/components/ListList.svelte
@@ -11,17 +11,23 @@
     useUpdateListsOrder,
   } from "@/hooks/useList";
   import DeleteButton from "./base/DeleteButton.svelte";
-  import { dndzone } from "svelte-dnd-action";
+  import { SHADOW_ITEM_MARKER_PROPERTY_NAME, dndzone } from "svelte-dnd-action";
   import type { ListsResponse } from "@/lib/types";
   import { flip } from "svelte/animate";
   import { flipDurationMs } from "@/lib/constants";
   import DragHandle from "./base/DragHandle.svelte";
+  import { fade } from "svelte/transition";
+  import { cubicIn } from "svelte/easing";
+
+  type ListWithShadowItem = ListsResponse & {
+    [SHADOW_ITEM_MARKER_PROPERTY_NAME]?: string;
+  };
 
   $: lists = useLists();
   $: createList = useCreateList();
   $: removeList = useRemoveList();
 
-  $: listData = $lists.data ?? [];
+  $: listData = ($lists.data ?? []) as ListWithShadowItem[];
 
   $: updateListOrder = useUpdateListsOrder();
   const handleConsider = (ev: CustomEvent<DndEvent<ListsResponse>>) => {
@@ -47,17 +53,17 @@
     type: "lists",
     flipDurationMs,
     dropTargetStyle: {},
-    dropTargetClasses: ["outline", "outline-1", "outline-primary"],
+    dropTargetClasses: ["border-primary"],
   }}
   on:consider={handleConsider}
   on:finalize={handleFinalize}
-  class="bg-card scroll-gutter max-h-[200px] overflow-y-auto rounded-md border py-2 pr-2"
+  class="bg-card max-h-[200px] overflow-y-auto rounded-md border py-2 transition-colors"
 >
   {#each listData as list (list.id)}
     <div
       animate:flip={{ duration: flipDurationMs }}
       class={cn(
-        "text-muted-foreground flex h-9 w-full items-center justify-between gap-2 pl-4 hover:border-l-4 hover:pl-3",
+        "text-muted-foreground relative flex h-9 w-full items-center justify-between gap-2 pl-4 pr-2 hover:border-l-4 hover:pl-3",
         !list.name && "italic",
         $location.includes(list.id) &&
           "border-primary text-foreground border-l-4 pl-3",
@@ -68,6 +74,12 @@
       </a>
       <DeleteButton handleDelete={() => $removeList.mutate(list.id)} />
       <DragHandle />
+      {#if list[SHADOW_ITEM_MARKER_PROPERTY_NAME]}
+        <div
+          in:fade={{ duration: 200, easing: cubicIn }}
+          class="bg-secondary/50 visible absolute inset-0"
+        />
+      {/if}
     </div>
   {/each}
 </div>
