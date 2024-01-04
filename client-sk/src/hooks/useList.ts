da2e1a592659ce2f058a995eac22b0d3c1e9898d
Riley Paul <rileypaul96@gmail.com>
2024-01-04 08:27:23 -0800

    update list queries

--- client-sk/src/hooks/useList.ts
+++ client-sk/src/hooks/useList.ts
@@ -1,131 +1,124 @@
-import { pb } from "@/lib/pocketbase";
-import { currentList } from "@/lib/store";
-import { createMutation, createQuery } from "@tanstack/svelte-query";
-import type { ClientResponseError } from "pocketbase";
+import { pb } from '@/lib/pocketbase';
+import { createMutation, createQuery } from '@tanstack/svelte-query';
+import type { ClientResponseError } from 'pocketbase';
 
-import { queryClient } from "@/lib/query";
+import { queryClient } from '@/lib/query';
 import {
-  type CategoriesItemsResponse,
-  type ListCategoriesResponse,
-  type ItemsResponse,
-  Collections,
-  type ListsResponse,
-} from "@/lib/types";
-import { goto } from "$app/navigation";
+	type CategoriesItemsResponse,
+	type ListCategoriesResponse,
+	type ItemsResponse,
+	type ListsResponse,
+	Collections
+} from '@/lib/types';
+import { goto } from '$app/navigation';
+import { page } from '$app/stores';
 
-const CATEGORY_ITEM_EXPAND_KEY = "categories_items(category)";
+const CATEGORY_ITEM_EXPAND_KEY = 'categories_items(category)';
 
 type CategoriesItemsExpandItem = CategoriesItemsResponse<{
-  item: ItemsResponse;
+	item: ItemsResponse;
 }>;
 
 type CategoriesExpandCategoriesItems = ListCategoriesResponse<{
-  [CATEGORY_ITEM_EXPAND_KEY]: CategoriesItemsExpandItem[];
+	[CATEGORY_ITEM_EXPAND_KEY]: CategoriesItemsExpandItem[];
 }>;
 
 export type ExpandedCategoryItem = CategoriesItemsExpandItem & {
-  itemData: ItemsResponse;
+	itemData: ItemsResponse;
 };
 
 export type ExpandedCategory = CategoriesExpandCategoriesItems & {
-  items: ExpandedCategoryItem[];
+	items: ExpandedCategoryItem[];
 };
 
 export type ListWithCategories = ListsResponse & {
-  categories: ExpandedCategory[];
+	categories: ExpandedCategory[];
 };
 
-const expandItems = (
-  record: CategoriesItemsExpandItem,
-): ExpandedCategoryItem => ({
-  ...record,
-  itemData: record.expand?.item ?? ({} as ItemsResponse),
+const expandItems = (record: CategoriesItemsExpandItem): ExpandedCategoryItem => ({
+	...record,
+	itemData: record.expand?.item ?? ({} as ItemsResponse)
 });
 
-const expandCategory = (
-  record: CategoriesExpandCategoriesItems,
-): ExpandedCategory => ({
-  ...record,
-  items:
-    record.expand?.[CATEGORY_ITEM_EXPAND_KEY]?.map(expandItems).sort(
-      (a: ExpandedCategoryItem, b: ExpandedCategoryItem) =>
-        a.sort_order - b.sort_order,
-    ) ?? [],
+const expandCategory = (record: CategoriesExpandCategoriesItems): ExpandedCategory => ({
+	...record,
+	items:
+		record.expand?.[CATEGORY_ITEM_EXPAND_KEY]?.map(expandItems).sort(
+			(a: ExpandedCategoryItem, b: ExpandedCategoryItem) => a.sort_order - b.sort_order
+		) ?? []
 });
 
 export const useList = (listId: string) =>
-  createQuery<ListWithCategories, ClientResponseError>({
-    queryKey: ["list", listId],
-    queryFn: async () => {
-      const [list, categories] = await Promise.all([
-        pb
-          .collection(Collections.Lists)
-          .getOne(listId ?? "", { requestKey: null }),
-        pb
-          .collection(Collections.ListCategories)
-          .getFullList<CategoriesExpandCategoriesItems>({
-            filter: `list = "${listId}"`,
-            sort: "sort_order",
-            expand: "categories_items(category).item",
-          })
-          .then((data) => data.map(expandCategory)),
-      ]);
-      return { ...list, categories };
-    },
-  });
+	createQuery<ListWithCategories, ClientResponseError>({
+		queryKey: [Collections.Lists, listId],
+		queryFn: async () => {
+			const [list, categories] = await Promise.all([
+				pb.collection(Collections.Lists).getOne(listId ?? '', { requestKey: null }),
+				pb
+					.collection(Collections.ListCategories)
+					.getFullList<CategoriesExpandCategoriesItems>({
+						filter: `list = "${listId}"`,
+						sort: 'sort_order',
+						expand: 'categories_items(category).item'
+					})
+					.then((data) => data.map(expandCategory))
+			]);
+			return { ...list, categories };
+		}
+	});
 
 export const useLists = () =>
-  createQuery<ListsResponse[], ClientResponseError>({
-    queryKey: ["lists"],
-    queryFn: () =>
-      pb.collection(Collections.Lists).getFullList({ sort: "sort_order" }),
-  });
+	createQuery<ListsResponse[], ClientResponseError>({
+		queryKey: [Collections.Lists],
+		queryFn: () => pb.collection(Collections.Lists).getFullList({ sort: 'sort_order' })
+	});
 
 export const useCreateList = () =>
-  createMutation({
-    mutationFn: () =>
-      pb.collection(Collections.Lists).create({
-        user: pb.authStore.model?.id,
-        sort_order:
-          queryClient.getQueryData<ListsResponse[]>(["lists"])?.length ?? 0,
-        weight_unit: "g",
-      }),
-    onSuccess: (data) => {
-      goto(`/${data.id}`);
-      queryClient.invalidateQueries({ queryKey: ["lists"] });
-    },
-  });
+	createMutation({
+		mutationFn: () => {
+			const currentLists = queryClient.getQueryData<ListsResponse[]>([Collections.Lists]);
+			const maxSortOrder = Math.max(...(currentLists?.map((list) => list.sort_order) ?? [0]));
+
+			return pb.collection(Collections.Lists).create({
+				user: pb.authStore.model?.id,
+				sort_order: maxSortOrder + 1,
+				weight_unit: 'g'
+			});
+		},
+		onSuccess: (data) => {
+			goto(`/${data.id}`);
+			queryClient.invalidateQueries({ queryKey: [Collections.Lists] });
+		}
+	});
 
 export const useRemoveList = () =>
-  createMutation({
-    mutationFn: (id: string) => pb.collection(Collections.Lists).delete(id),
-    onSuccess: (_, variables) =>
-      currentList.subscribe((listId) => {
-        if (listId === variables) goto("/");
-        queryClient.invalidateQueries({ queryKey: ["lists"] });
-      }),
-  });
+	createMutation({
+		mutationFn: (id: string) => pb.collection(Collections.Lists).delete(id),
+		onSuccess: (_, variables) =>
+			page.subscribe(({ params: { listId } }) => {
+				if (listId === variables) goto('/');
+				queryClient.invalidateQueries({ queryKey: [Collections.Lists] });
+			})
+	});
 
 export const useUpdateList = () =>
-  createMutation({
-    mutationFn: (variables: {
-      id: string;
-      list: Partial<ListWithCategories>;
-    }) => pb.collection(Collections.Lists).update(variables.id, variables.list),
-    onSuccess: () =>
-      currentList.subscribe((listId) => {
-        queryClient.invalidateQueries({ queryKey: ["list", listId] });
-        queryClient.invalidateQueries({ queryKey: ["lists"] });
-      }),
-  });
+	createMutation({
+		mutationFn: (variables: { id: string; list: Partial<ListWithCategories> }) =>
+			pb.collection(Collections.Lists).update(variables.id, variables.list),
+		onSuccess: () =>
+			page.subscribe(({ params: { listId } }) => {
+				queryClient.invalidateQueries({ queryKey: [Collections.Lists, listId] });
+				queryClient.invalidateQueries({ queryKey: [Collections.Lists] });
+			})
+	});
 
 export const useUpdateListsOrder = () =>
-  createMutation({
-    mutationFn: (variables: { listIds: string[] }) =>
-      Promise.all(
-        variables.listIds.map((id, index) =>
-          pb.collection(Collections.Lists).update(id, { sort_order: index }),
-        ),
-      ),
-    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["lists"] }),
-  });
+	createMutation({
+		mutationFn: (variables: { listIds: string[] }) =>
+			Promise.all(
+				variables.listIds.map((id, index) =>
+					pb.collection(Collections.Lists).update(id, { sort_order: index })
+				)
+			),
+		onSuccess: () => queryClient.invalidateQueries({ queryKey: [Collections.Lists] })
+	});
