c953d5573ee1a4f3b96e8824ade426c622b595f2
Riley Paul <rileypaul96@gmail.com>
2024-01-11 07:11:45 -0800

    add lots of images

--- client/src/routes/(app)/[listId]/ItemImage.svelte
+++ client/src/routes/(app)/[listId]/ItemImage.svelte
@@ -8,27 +8,37 @@
 		DialogTitle,
 		DialogTrigger
 	} from '@/components/ui/dialog';
-	import { Loader2, Save, Trash } from 'lucide-svelte';
+	import { Trash } from 'lucide-svelte';
 	import { Button } from '@/components/ui/button';
 	import { cn } from '@/lib/utils';
 	import { useDeleteItemImage, useSetItemImage } from '@/hooks/useItem';
 	import type { ItemsResponse } from '@/lib/types';
 	import { pb } from '@/lib/pocketbase';
+	import { onDestroy, onMount } from 'svelte';
+	import { Input } from '@/components/ui/input';
 
 	export let item: ItemsResponse;
 
 	let isOpen = false;
-	let uploadedFiles: FileList | undefined;
 
 	const setItemImage = useSetItemImage();
 	const deleteItemImage = useDeleteItemImage();
 
-	$: saveItem = () => {
-		if (uploadedFiles) {
-			$setItemImage.mutate({ id: item.id, image: uploadedFiles[0] });
+	const pasteFiles = (e: ClipboardEvent) => {
+		if (isOpen && e.clipboardData?.files?.length) {
+			$setItemImage.mutate({ id: item.id, image: e.clipboardData.files[0] });
 		}
-		isOpen = false;
 	};
+
+	const handleInputChange = (e: Event) => {
+		const target = e.target as HTMLInputElement;
+		if (target.files) {
+			$setItemImage.mutate({ id: item.id, image: target.files[0] });
+		}
+	};
+
+	onMount(() => window.addEventListener('paste', pasteFiles));
+	onDestroy(() => window.removeEventListener('paste', pasteFiles));
 </script>
 
 <Dialog closeOnEscape bind:open={isOpen}>
@@ -54,53 +64,36 @@
 			<DialogTitle>Update {item.name} Image</DialogTitle>
 			<DialogDescription>Upload or provide a link to an image</DialogDescription>
 		</DialogHeader>
-		<form on:submit={saveItem} class="grid gap-4">
-			<input
-				type="file"
-				accept="image/*"
-				bind:files={uploadedFiles}
-				class="border-input file:text-foreground placeholder:text-muted-foreground focus-visible:ring-ring flex h-9 w-full rounded-md border bg-inherit px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50"
-			/>
+		<Input type="file" accept="image/*" on:change={handleInputChange} />
 
-			<div
-				class={cn(
-					'text-muted-foreground flex aspect-square items-center justify-center rounded-md p-2',
-					item.image || uploadedFiles ? 'bg-white' : 'bg-muted'
-				)}
+		<div
+			class={cn(
+				'text-muted-foreground flex aspect-square items-center justify-center rounded-md p-2',
+				item.image ? 'bg-white' : 'bg-muted'
+			)}
+		>
+			{#if item.image}
+				<img
+					class="h-full w-full object-contain"
+					src={pb.files.getUrl(item, item.image, { thumb: '500x500' })}
+					alt={item.name}
+				/>
+			{:else}
+				No Image
+			{/if}
+		</div>
+
+		<DialogFooter>
+			<Button
+				type="button"
+				variant="destructive"
+				disabled={$deleteItemImage.isPending}
+				on:click={() => $deleteItemImage.mutate(item.id)}
 			>
-				{#if item.image || uploadedFiles}
-					<img
-						class="h-full w-full object-contain"
-						src={uploadedFiles
-							? URL.createObjectURL(uploadedFiles[0])
-							: pb.files.getUrl(item, item.image, { thumb: '500x500' })}
-						alt={item.name}
-					/>
-				{:else}
-					No Image
-				{/if}
-			</div>
-
-			<DialogFooter>
-				<Button
-					type="button"
-					variant="destructive"
-					disabled={$deleteItemImage.isPending}
-					on:click={() => $deleteItemImage.mutate(item.id)}
-				>
-					<Trash class="mr-2 h-4 w-4" />
-					Delete Image
-				</Button>
-				<Button type="submit" disabled={$setItemImage.isPending}>
-					{#if $setItemImage.isPending}
-						<Loader2 class="mr-2 h-4 w-4 animate-spin" />
-					{:else}
-						<Save class="mr-2 h-4 w-4" />
-					{/if}
-					Save
-				</Button>
-			</DialogFooter>
-			<input type="hidden" />
-		</form>
+				<Trash class="mr-2 h-4 w-4" />
+				Delete Image
+			</Button>
+		</DialogFooter>
+		<input type="hidden" />
 	</DialogContent>
 </Dialog>
