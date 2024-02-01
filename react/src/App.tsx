99e2b56e4ba151df2e6adc1b261a2c1735b2dce8
Riley Paul <rileypaul96@gmail.com>
2024-02-01 08:00:14 -0800

    add router starter code

--- react/src/App.tsx
+++
@@ -1,6 +0,0 @@
-import { Button } from "./components/ui/button";
-import { toast } from "sonner";
-
-export default function App() {
-  return <Button onClick={() => toast("Hello world")}>Hello</Button>;
-}
--- react/src/main.tsx
+++ react/src/main.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { Toaster } from "./components/ui/sonner.tsx";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-    <Toaster />
-  </React.StrictMode>
-);
+// Import the generated route tree
+import { routeTree } from "./routeTree.gen";
+
+// Create a new router instance
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+// Render the app
+const rootElement = document.getElementById("app");
+if (rootElement && !rootElement?.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
+}
