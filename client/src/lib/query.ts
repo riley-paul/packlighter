c6d8258f6f9f85dd7b2064de70050b74a3a0e6c5
Riley Paul <rileypaul96@gmail.com>
2024-02-01 07:29:15 -0800

    add react project

--- client/src/lib/query.ts
+++ client/src/lib/query.ts
@@ -1,3 +1,21 @@
-import { QueryClient } from "@tanstack/svelte-query";
+import { QueryClient, createMutation } from '@tanstack/svelte-query';
+import { pb } from './pocketbase';
+import { Collections } from './types';
+import { zSchema } from './schema';
+import { toast } from 'svelte-sonner';
 
 export const queryClient = new QueryClient();
+
+export const initSchemaMutation = createMutation({
+	mutationFn: (userId: string) => {
+		const currentSchema = pb.authStore.model?.schema ?? {};
+		return pb.collection(Collections.Users).update(userId, {
+			schema: zSchema.parse(currentSchema)
+		});
+	},
+	onSuccess: () => toast.success('Schema initialized'),
+	onError: (err) => {
+		toast.error('Error initializing schema');
+		console.log(err);
+	}
+});
