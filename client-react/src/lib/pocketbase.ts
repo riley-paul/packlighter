ad3d57eb4e229fbd817cd3f4b5cac00e4de9acd3
Riley Paul <71047303+rjp301@users.noreply.github.com>
2024-03-06 07:57:41 -0800

    Delete client-react directory

--- client-react/src/lib/pocketbase.ts
+++
@@ -1,8 +0,0 @@
-import PocketBase from "pocketbase";
-import type { TypedPocketBase } from "./types";
-
-const pb = new PocketBase(
-  "https://packlighter.rileypaul.ca"
-) as TypedPocketBase;
-
-export default pb;
