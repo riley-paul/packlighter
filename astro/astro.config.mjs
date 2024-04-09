93b5398bcc54331b488abedf07f90380925899f2
Riley Paul <rileypaul96@gmail.com>
2024-04-09 17:30:33 -0600

    add clerk auth

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -3,13 +3,21 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 import vercel from "@astrojs/vercel/serverless";
+import clerk from "astro-clerk-auth";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   prefetch: true,
-  integrations: [react(), tailwind({
-    applyBaseStyles: false
-  })],
-  adapter: vercel()
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    clerk({
+      afterSignInUrl: "/",
+      afterSignUpUrl: "/",
+    }),
+  ],
+  adapter: vercel(),
 });
