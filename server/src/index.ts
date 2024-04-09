f3ce48395aed8735182d96bd65f7f324ab0459c2
Riley Paul <rileypaul96@gmail.com>
2024-04-09 09:31:17 -0600

    add routes and clerk middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,14 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 
-const app = new Hono()
+import lists from "./routes/lists";
+import items from "./routes/items";
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const app = new Hono();
 
-export default app
+app.use("*", clerkMiddleware());
+
+const routes = app.route("/lists", lists).route("/items", items);
+
+export type AppType = typeof routes;
+export default app;
---
+++ server/src/routes/lists.ts
@@ -0,0 +1,20 @@
+import { getAuth } from "@hono/clerk-auth";
+import { Hono } from "hono";
+
+const app = new Hono()
+  .get("/", (c) => {
+    const auth = getAuth(c);
+
+    if (!auth?.userId) {
+      c.status(401);
+      return c.text("Unauthorized");
+    }
+
+    return c.text("all lists");
+  })
+  .post("/", (c) => c.text("create list"))
+  .get("/:id", (c) => c.text(`list ${c.req.param("id")}`))
+  .put("/:id", (c) => c.text(`update list ${c.req.param("id")}`))
+  .delete("/:id", (c) => c.text(`delete list ${c.req.param("id")}`));
+
+export default app;
