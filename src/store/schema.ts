9fcbd98e30f332e4ec5f18c7c6840ff3daea0f25
Riley Paul <rileypaul96@gmail.com>
2024-04-12 11:16:50 -0600

    define schema

---
+++ src/store/schema.ts
@@ -0,0 +1,44 @@
+import { z } from "zod";
+import { v4 as uuid } from "uuid";
+
+export const weightUnits = ["g", "kg", "lb", "oz"] as const;
+export const zWeightUnits = z.enum(weightUnits);
+
+export const zItem = z.object({
+  id: z.string().default(() => uuid()),
+  name: z.string().default(""),
+  description: z.string().default(""),
+  weight: z.number().default(0),
+  weightUnit: zWeightUnits.default("g"),
+  imageUrl: z.string().optional(),
+});
+export type Item = z.infer<typeof zItem>;
+
+export const zCategoryItem = z.object({
+  id: z.string().default(() => uuid()),
+  itemId: z.string(),
+  quantity: z.number().default(1),
+  packed: z.boolean().default(false),
+});
+export type CategoryItem = z.infer<typeof zCategoryItem>;
+
+export const zCategory = z.object({
+  id: z.string().default(() => uuid()),
+  name: z.string().default(""),
+  items: z.array(zCategoryItem).default([]),
+});
+export type Category = z.infer<typeof zCategory>;
+
+export const zList = z.object({
+  id: z.string().default(() => uuid()),
+  name: z.string().default(""),
+  description: z.string().default(""),
+  categories: z.array(zCategory).default([]),
+});
+export type List = z.infer<typeof zList>;
+
+export const zState = z.object({
+  lists: z.array(zList).default([]),
+  items: z.array(zItem).default([]),
+});
+export type State = z.infer<typeof zState>;
