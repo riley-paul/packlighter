6e15d60cccd32a6424938b2d6344969c734a622d
Riley Paul <rileypaul96@gmail.com>
2024-05-24 09:33:48 -0600

    remove edge runtime

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,8 +1,8 @@
 import app from "@/index";
 import { handle } from "hono/vercel";
 
-export const config = {
-  runtime: "edge",
-};
+// export const config = {
+//   runtime: "edge",
+// };
 
 export default process.env.VERCEL ? handle(app) : app;
