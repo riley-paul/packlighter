1174ead2ec543eb362ea6e01d6382afc22ec243e
Riley Paul <rileypaul96@gmail.com>
2024-05-24 09:19:39 -0600

    remove type errors in api

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,6 @@
 import { Hono } from "hono";
 
-import stationRoutes from "./routes/stations";
 import authRoutes from "./routes/auth";
-import settingsRoutes from "./routes/settings";
-import emailsRoutes from "./routes/emails";
-import weatherRoutes from "./routes/weather";
 
 export const config = {
   runtime: "edge",
@@ -14,10 +10,6 @@ const app = new Hono().basePath("/api");
 
 const routes = app
   .route("/auth", authRoutes)
-  .route("/emails", emailsRoutes)
-  .route("/settings", settingsRoutes)
-  .route("/stations", stationRoutes)
-  .route("/weather", weatherRoutes)
   .get("/", (c) => c.json({ message: "Hello Hono!" }));
 
 export default app;
